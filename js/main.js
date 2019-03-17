842d9919f92c1ec747270bc77b63af015d9d2973
Julia Ortis <777ortis@gmail.com>
2019-03-17 19:36:05 +0200

    html-code

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('select').styler();
+    $("a.topLink").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
+});
